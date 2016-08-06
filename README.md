78f4116301b0cfe4ffe5eddf9ae115c5489ce8d0
Davide Icardi <davide.icardi@gmail.com>
2016-08-06 15:44:09 +0200

    added a sample on how to receive notifications

--- README.md
+++ README.md
@@ -8,6 +8,13 @@
 
 See `./sample/` directory.
 
+### How to run a sample
+
+    npm install # install dependencies
+    set FORGE_TARGET=yourtarget
+    # ensure to have a valid configuration file "config.yourtarget.json" 
+    node ./sample/waitForNotifications.js
+
 ## How to publish new version
 
     git commit -a -m "message"
---
+++ sample/waitForNotifications.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const sdk = require("./../index.js");
+const ForgeNotificationBus = sdk.ForgeNotificationBus;
+
+const config = require("./../config.js");
+
+let notificationBus = new ForgeNotificationBus(config.serviceBus);
+
+function connect(){
+	return notificationBus.startReceiving();
+}
+// function disconnect(){
+// 	return notificationBus.stopReceiving();
+// }
+
+connect()
+.then(() => {
+
+	console.log("Waiting for an entity to be published/unpublished:");
+	notificationBus.on("EntityDistributionNotification", (e) => {
+		if (e.action === "publish")	console.log(`Entity ${e.record.slug} published`);
+		if (e.action === "unpublish")	console.log(`Entity ${e.record.slug} unpublished`);
+	});
+
+});
