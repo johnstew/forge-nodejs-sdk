6bf52018ee63b919273919cf60b8fb20e2ed3156
Davide Icardi <davide.icardi@gmail.com>
2017-03-16 13:25:48 +0100

    Sample notification

--- sample/waitForDistNotifications.js
+++ sample/waitForDistNotifications.js
@@ -6,17 +6,18 @@
 */
 
 const sdk = require("./../index.js"); // forge-nodejs-sdk
-const ForgeNotificationBus = sdk.ForgeNotificationBus;
 const DistributionNotificationBus = sdk.DistributionNotificationBus;
 
 const config = require("./../config.js");
 
-let notificationBus = new ForgeNotificationBus(config.serviceBus);
 let distNotificationBus = new DistributionNotificationBus(config.serviceBus);
+distNotificationBus.on("error", console.error);
+distNotificationBus.on("_connectionStatusChanged", (s) => {
+	console.log(s.name, s.connected ? "-> CONNECTED" : "-> DISCONNECTED " + s.error);
+});
 
 function connect(){
-	return notificationBus.startReceiving()
-	.then(() => distNotificationBus.startReceiving());
+	return distNotificationBus.startReceiving();
 }
 // function disconnect(){
 // 	return notificationBus.stopReceiving();
@@ -26,7 +27,6 @@ connect()
 .then(() => {
 
 	distNotificationBus.on("error", console.error);
-	notificationBus.on("error", console.error);
 
 	console.log("Waiting for an entity to be published/unpublished:");
 	distNotificationBus.on("EntityDistributionNotification", (e) => {
@@ -35,21 +35,6 @@ connect()
 
 		if (e.action === "unpublish")
 			console.log(`Distribution ${e.entityType} ${e.slug} unpublished`);
-	});
-
-	// notificationBus.on("CommandSuccessNotification", (e) => {
-	// 	console.log(`CommandSuccessNotification ${e.commandId}`);
-	// });
-
-	notificationBus.on("PublishedBatchNotification", (e) => {
-		console.log(`Forge PublishedBatchNotification (Priority:${e.messagePriority})`);
-	});
-
-	notificationBus.on("PublishedNotification", (e) => {
-		console.log(`Forge PublishedNotification ${e.translationKeys[0].slug} (Priority:${e.messagePriority})`);
-	});
-	notificationBus.on("UnpublishedNotification", (e) => {
-		console.log(`Forge UnpublishedNotification ${e.translationKeys[0].slug} (Priority:${e.messagePriority})`);
 	});
 
 })
