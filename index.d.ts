8aa691f107ab10589a028b805feca659e589ebdf
Enrico Massone <enrico.massone@deltatre.it>
2017-11-30 13:23:05 +0100

    Fixed the export inside index.ts missing enum has been added to exports

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 export { ForgeManagementApi, IForgeManagementApiOptions } from "./src/ForgeManagementApi";
 export { ForgeNotificationBus, IForgeNotificationBusOptions } from "./src/ForgeNotificationBus";
 export { DistributionNotificationBus, IDistributionNotificationBusOptions } from "./src/DistributionNotificationBus";
-export { ForgeDistributionApi, IForgeDistributionApiOptions } from "./src/ForgeDistributionApi";
+export { ForgeDistributionApi, IForgeDistributionApiOptions, ReadSource } from "./src/ForgeDistributionApi";
 export { ForgeFrontEndApi, IForgeFrontEndApiOptions } from "./src/ForgeFrontEndApi";
 import * as ForgeCommands from "./src/ForgeCommands";
 export { ForgeCommands };
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var DistributionNotificationBus_1 = require("./src/DistributionNotificationBus")
 exports.DistributionNotificationBus = DistributionNotificationBus_1.DistributionNotificationBus;
 var ForgeDistributionApi_1 = require("./src/ForgeDistributionApi");
 exports.ForgeDistributionApi = ForgeDistributionApi_1.ForgeDistributionApi;
+exports.ReadSource = ForgeDistributionApi_1.ReadSource;
 var ForgeFrontEndApi_1 = require("./src/ForgeFrontEndApi");
 exports.ForgeFrontEndApi = ForgeFrontEndApi_1.ForgeFrontEndApi;
 const ForgeCommands = require("./src/ForgeCommands");
